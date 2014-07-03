69502d05c048240304d268dbee2e9a4e5b7c1b58
Ryan Whitmire <ryanw51@gmail.com>
2014-07-02 21:39:41 -0400

    building toNumber functionality

--- src/dehumanizer.js
+++ src/dehumanizer.js
@@ -2,11 +2,66 @@
 
 	var root = this;
 
+	var numvals = {
+		'one': 1,
+		'two': 2,
+		'three': 3,
+		'four': 4,
+		'five': 5,
+		'six': 6,
+		'seven': 7,
+		'eight': 8,
+		'nine': 9,
+		'ten': 10,
+		'eleven': 11,
+		'twelve': 12,
+		'thirteen': 13,
+		'fourteen': 14,
+		'fifteen': 15,
+		'sixteen': 16,
+		'seventeen': 17,
+		'eighteen': 18,
+		'nineteen': 19,
+		'twenty': 20,
+		'thirty': 30,
+		'fourty': 40,
+		'fifty': 50,
+		'sixty': 60,
+		'seventy': 70,
+		'eighty': 80,
+		'ninety': 90
+	};
+
+	var multipliers = {
+		'hundred': 100,
+		'thousand': 1000,
+		'million': 1000000,
+		'billion': 1000000000
+	}
+
 	var dehumanizer = function(humanValue){
+		if(humanValue !== undefined){
+			humanValue = humanValue.toLowerCase();
+		}
+
 		this.toBoolean = function(){
 			if(humanValue == 'true') return true;
 			return false;
 		};
+
+		this.toNumber = function(){
+
+			var tokens = humanValue.split(' ');
+
+			
+
+			var val = numvals[humanValue];
+			return val || NaN;
+		};
+
+		this.toDate = function(){
+			return new Date();
+		}
 
 		return this;
 	};
--- test/dehumanizer.test.js
+++ test/dehumanizer.test.js
@@ -7,5 +7,54 @@ describe('dehumanizer', function () {
 			var result = dehumanizer('true').toBoolean();
 			result.should.be.true;
 		});
+
+		it('should return false with string "false"', function () {
+			var result = dehumanizer('false').toBoolean();
+			result.should.be.false;
+		});
+	});
+
+	describe('toNumber', function () {
+		it('should return 1 with string "one"', function () {
+			var result = dehumanizer('one').toNumber();
+			result.should.equal(1);
+		});
+
+		it('should return 16 with string "sixteen"', function () {
+			var result = dehumanizer('sixteen').toNumber();
+			result.should.equal(16);
+		});
+
+		it('should return 16 with string "SIXteen"', function () {
+			var result = dehumanizer('SIXteen').toNumber();
+			result.should.equal(16);
+		});
+
+		it('should return 116 with string "one hundred sixteen"', function () {
+			var result = dehumanizer('one hundred sixteen').toNumber();
+			result.should.equal(116);
+		});
+
+		// it('should return 216 with string "two hundred sixteen"', function () {
+		// 	var result = dehumanizer('two hundred sixteen').toNumber();
+		// 	result.should.equal(216);
+		// });
+
+		// it('should return 3216 with string "three thousand two hundred sixteen"', function () {
+		// 	var result = dehumanizer('three thousand two hundred sixteen').toNumber();
+		// 	result.should.equal(3216);
+		// });
+
+		it('should return NaN with rubbish', function () {
+			var result = dehumanizer('rubbish').toNumber();
+			result.should.be.NaN;
+		});
+	});
+
+	describe('toDate', function () {
+		it('should return a date type for "tomorrow"', function () {
+			var result = dehumanizer('tomorrow').toDate();
+			result.should.be.a('date');
+		});
 	});
 });
